a9d379ac61f878930c9bcd561c9020d63c99709b
aryanpatel287 <aryanpatel.me@gmail.com>
2026-03-09 09:47:02 +0530

    feat: initialize Day91 project with Express and MongoDB setup

---
+++ Day91/server.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+const app = require('./src/app')
+const connectToDb= require('./config/database')
+connectToDb()
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000')
+})
